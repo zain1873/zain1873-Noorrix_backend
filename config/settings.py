16e25ce258f1a339cfd9ba9c68641b27fcddbac3
Zain Saleem <zain.saleem155@gmail.com>
2026-06-04 17:13:39 +0500

    updated

--- config/settings.py
+++ config/settings.py
@@ -107,9 +107,15 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': False,
 }
 
-EMAIL_BACKEND = 'anymail.backends.resend.EmailBackend'
-ANYMAIL = {'RESEND_API_KEY': config('RESEND_API_KEY', default='')}
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='onboarding@resend.dev')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_PORT = config('EMAIL_PORT', default=465, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
+EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@noorrix.com')
+EMAIL_TIMEOUT = 10
 FRONTEND_URL = config('FRONTEND_URL', default='http://localhost:3000')
 
 STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY', default='')
