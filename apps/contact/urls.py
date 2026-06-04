3f754a27d10f5615001f143df21a6e90f5852c0e
Zain Saleem <zain.saleem155@gmail.com>
2026-06-05 00:41:33 +0500

    changes revert

--- apps/contact/urls.py
+++ apps/contact/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import ContactSubmissionView, EmailTestView
+from .views import ContactSubmissionView
 
 urlpatterns = [
     path("contact/", ContactSubmissionView.as_view(), name="contact-submit"),
-    path("email-test/", EmailTestView.as_view(), name="email-test"),
 ]
