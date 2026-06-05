d208241d2eb2c553692f010e134ee9134dfd9449
Zain Saleem <zain.saleem155@gmail.com>
2026-06-05 12:57:17 +0500

    fix: add explicit AUTHENTICATION_BACKENDS for admin login

--- config/settings.py
+++ config/settings.py
@@ -80,6 +80,10 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
