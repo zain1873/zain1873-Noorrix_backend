4b55a6342cf8a3672274337e187af06a3e3492e2
Zain Saleem <zain.saleem155@gmail.com>
2026-06-05 13:29:47 +0500

    fix: add ensure_admin command to startup

---
+++ apps/auth/management/commands/ensure_admin.py
@@ -0,0 +1,28 @@
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+
+class Command(BaseCommand):
+    help = 'Ensure admin user exists with correct credentials'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@noorrix.com')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', '')
+        name = os.environ.get('DJANGO_SUPERUSER_NAME', 'Admin')
+
+        if not password:
+            self.stdout.write('DJANGO_SUPERUSER_PASSWORD not set, skipping.')
+            return
+
+        user, created = User.objects.get_or_create(email=email, defaults={'name': name})
+        user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.is_active = True
+        user.name = name
+        user.save()
+
+        action = 'Created' if created else 'Updated'
+        self.stdout.write(f'{action} admin: {email}')
