fcea06e7b85a7625e14f3ccae0b27b60b1040e0a
Zain Saleem <zain.saleem155@gmail.com>
2026-06-06 21:40:24 +0500

    security: remove debug_auth endpoint before production merge

--- config/urls.py
+++ config/urls.py
@@ -1,47 +1,5 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.contrib.auth import authenticate, get_user_model
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
-import json
 from django.urls import path, include
-
-
-@csrf_exempt
-@require_POST
-def debug_auth(request):
-    data = json.loads(request.body)
-    email = data.get('email')
-    password = data.get('password')
-    User = get_user_model()
-    try:
-        user = User.objects.get(email=email)
-        return JsonResponse({
-            'user_exists': True,
-            'is_staff': user.is_staff,
-            'is_superuser': user.is_superuser,
-            'is_active': user.is_active,
-            'check_password': user.check_password(password),
-            'authenticate': str(authenticate(request, username=email, password=password)),
-        })
-    except User.DoesNotExist:
-        return JsonResponse({'user_exists': False})
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
@@ -50,7 +8,6 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/debug-auth/', debug_auth),
     path('api/v1/auth/', include('apps.auth.urls')),
     path('api/v1/payments/', include('apps.payments.urls')),
     path('api/', include('apps.contact.urls')),
